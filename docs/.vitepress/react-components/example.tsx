b95945900b51c6832e40723b079abd9295b6a017
vjee <vjee2e@gmail.com>
2022-10-04 22:11:29 +0200

    Add basic example to getting started doc

---
+++ docs/.vitepress/react-components/example.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Form, useForm, useFormField, useFormContext } from '@formulier/react'
+
+export default function Example() {
+	const form = useForm({ firstName: '', lastName: '', email: '' })
+
+	return (
+		<Form
+			form={form}
+			onSubmit={values => {
+				alert(JSON.stringify(values, null, 2))
+			}}
+		>
+			<InputField name="firstName" label="First name" />
+			<InputField name="lastName" label="Last name" />
+			<InputField name="email" label="Email" type="email" />
+
+			<button type="submit">Submit</button>
+		</Form>
+	)
+}
+
+function InputField({ name, label, type = 'text' }) {
+	const form = useFormContext()
+	const [field] = useFormField(form, { name })
+
+	return (
+		<div>
+			<label htmlFor={name}>{label}</label>
+			<input {...field} type={type} />
+		</div>
+	)
+}
