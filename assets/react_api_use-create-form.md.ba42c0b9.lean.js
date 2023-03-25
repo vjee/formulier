50133108566032dbe195cd3be4403f62a279a654
vjee <vjee@users.noreply.github.com>
2023-03-25 19:28:43 +0000

    deploy: d86c36397c466233a9bd3cf868a408e241fc1509

--- assets/react_api_use-create-form.md.ba42c0b9.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,d as o}from"./app.3899738d.js";const B=JSON.parse('{"title":"useCreateForm()","description":"","frontmatter":{},"headers":[],"relativePath":"react/api/use-create-form.md"}'),p={name:"react/api/use-create-form.md"},e=o("",2),l=[e];function t(r,c,D,F,y,A){return n(),a("div",null,l)}const _=s(p,[["render",t]]);export{B as __pageData,_ as default};
