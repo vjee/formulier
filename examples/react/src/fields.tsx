0ad47a16c5b0126e7c9ecb8576e393c92e51439b
vjee <vjee2e@gmail.com>
2024-02-08 21:05:16 +0100

    Make example fields more accessible

--- examples/react/src/fields.tsx
+++ examples/react/src/fields.tsx
@@ -1,7 +1,7 @@
 import {useFormInstance, useFormField} from '@formulier/react'
 import * as React from 'react'
 
-export function TextField({name, label}: {name: string; label: string}) {
+export function TextField({name, label, info = null}: {name: string; info?: string | null; label: string}) {
 	const form = useFormInstance()
 	const [field, meta] = useFormField(form, {name})
 
@@ -9,7 +9,7 @@ export function TextField({name, label}: {name: string; label: string}) {
 	const {error} = meta
 
 	return (
-		<Field id={id} label={label} error={error}>
+		<Field id={id} label={label} error={error} info={info}>
 			<input
 				type="text"
 				id={id}
@@ -17,6 +17,8 @@ export function TextField({name, label}: {name: string; label: string}) {
 				value={value || ''}
 				onChange={event => onChange(event.target.value)}
 				onBlur={onBlur}
+				aria-describedby={!error ? `${id}-info` : `${id}-info ${id}-error`}
+				aria-invalid={!!error}
 			/>
 		</Field>
 	)
@@ -39,6 +41,8 @@ export function IntegerField({name, label}: {name: string; label: string}) {
 				value={value || ''}
 				onChange={event => onChange(parseInt(event.target.value, 10))}
 				onBlur={onBlur}
+				aria-describedby={!error ? `${id}-info` : `${id}-info ${id}-error`}
+				aria-invalid={!!error}
 			/>
 		</Field>
 	)
@@ -53,7 +57,15 @@ export function SelectField({name, label, children}: {name: string; label: strin
 
 	return (
 		<Field id={id} label={label} error={error}>
-			<select id={id} name={name} value={value || ''} onChange={event => onChange(event.target.value)} onBlur={onBlur}>
+			<select
+				id={id}
+				name={name}
+				value={value || ''}
+				onChange={event => onChange(event.target.value)}
+				onBlur={onBlur}
+				aria-describedby={!error ? `${id}-info` : `${id}-info ${id}-error`}
+				aria-invalid={!!error}
+			>
 				{children}
 			</select>
 		</Field>
@@ -76,6 +88,8 @@ export function CheckboxField({name, label}: {name: string; label: string}) {
 				checked={value || false}
 				onChange={event => onChange(event.target.checked)}
 				onBlur={onBlur}
+				aria-describedby={!error ? `${id}-info` : `${id}-info ${id}-error`}
+				aria-invalid={!!error}
 			/>
 		</Field>
 	)
@@ -85,22 +99,29 @@ export function Field({
 	id,
 	label,
 	error,
+	info,
 	children,
 	displayHorizontal = false,
 }: {
 	id: string
 	label: string
 	error: string | null
+	info?: string | null
 	children: React.ReactNode
 	displayHorizontal?: boolean
 }) {
 	return (
 		<div className={`field ${displayHorizontal ? 'horizontal' : ''}`}>
-			<label className="field-label" htmlFor={id}>
-				{label}
-			</label>
-			<div className="field-input">{children}</div>
-			{error ? <span className="field-error">{error}</span> : null}
+			<label htmlFor={id}>{label}</label>
+			<br />
+			{children}
+
+			{error ? (
+				<p id={`${id}-error`} role="alert">
+					{error}
+				</p>
+			) : null}
+			{info ? <p id={`${id}-info`}>{info}</p> : null}
 		</div>
 	)
 }
