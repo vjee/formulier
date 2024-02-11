981a1e112c19197e9dfc288b584df32ee5be2701
vjee <vjee2e@gmail.com>
2024-02-11 23:01:27 +0100

    Memoize context value in FormProvider

---
+++ .changeset/tricky-birds-switch.md
@@ -0,0 +1,5 @@
+---
+"@formulier/react": patch
+---
+
+Memoize context value in FormProvider
--- packages/react/src/form.ts
+++ packages/react/src/form.ts
@@ -10,7 +10,8 @@ import type {FormContext, FormProviderProps, Selector} from './types.js'
 const formContext = React.createContext<FormContext<Values, Primitives> | undefined>(undefined)
 
 function FormProvider<V extends Values, P extends Primitives = Primitives>({form, children}: FormProviderProps<V, P>) {
-	return React.createElement(formContext.Provider, {value: {form: form as Formulier<Values>}}, children)
+	const value = React.useMemo(() => ({form: form as Formulier<Values>}), [form])
+	return React.createElement(formContext.Provider, {value}, children)
 }
 
 function useFormInstance<V extends Values, P extends Primitives = Primitives>() {
