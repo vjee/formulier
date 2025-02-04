ebde9c2577fcf8f028a3e9801d8e79888efc6df1
Jasper Van Gestel <vjee2e@gmail.com>
2025-02-04 21:39:26 +0100

    Update cypress config

--- packages/react/cypress.config.ts
+++ packages/react/cypress.config.ts
@@ -3,6 +3,7 @@ import {defineConfig} from 'cypress'
 export default defineConfig({
 	video: false,
 	component: {
+		specPattern: 'cypress/**/*.cy.tsx',
 		devServer: {
 			framework: 'react',
 			bundler: 'vite',
