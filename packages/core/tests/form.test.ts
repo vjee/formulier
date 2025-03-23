2d27ddf08d72c9c045579d45c0e930fdb78c6348
Jasper Van Gestel <vjee2e@gmail.com>
2025-03-23 13:55:00 +0100

    Ensure all it() calls are in describe() calls

--- packages/core/tests/form.test.ts
+++ packages/core/tests/form.test.ts
@@ -1,136 +1,141 @@
-import {expect, it, vi} from 'vitest'
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import {describe, expect, it, vi} from 'vitest'
 
 import {Formulier} from '../src/form.js'
 import {getPath} from '../src/state-utils.js'
 
 const INITIAL_VALUES = {a: {b: {c: 'c', d: 'd'}}}
 
-it('copies initial values', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	expect(form.store.getState().values).toBe(INITIAL_VALUES)
-})
-
-it('calls listeners', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	const listener = vi.fn(() => undefined)
-	form.store.subscribe(listener)
-	form.setFieldValue('a.b.c', 'd')
-	expect(listener).toHaveBeenCalledTimes(1)
-})
-
-it('removes listeners', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	const listener = vi.fn(() => undefined)
-	const cancel = form.store.subscribe(listener)
-	cancel()
-	form.setFieldValue('a.b.c', 'd')
-	expect(listener).toHaveBeenCalledTimes(0)
-	expect(form.store['listeners'].size).toBe(0)
-})
-
-it('touches fields', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	expect(form.store.getState().touched['a.b.c']).toBe(undefined)
-	form.touchField('a.b.c')
-	expect(form.store.getState().touched['a.b.c']).toBe(true)
-})
-
-it('increments submit count', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	expect(form.store.getState().submitCount).toBe(0)
-	form.incrementSubmitCount()
-	form.incrementSubmitCount()
-	expect(form.store.getState().submitCount).toBe(2)
-})
-
-it('should not call listeners if setFieldValue does nothing', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	const listener = vi.fn(() => undefined)
-	form.store.subscribe(listener)
-	expect(listener).toHaveBeenCalledTimes(0)
-	form.setFieldValue('a.b.c', 'd')
-	expect(listener).toHaveBeenCalledTimes(1)
-	form.setFieldValue('a.b.c', 'd')
-	expect(listener).toHaveBeenCalledTimes(1)
-})
-
-it('should not call listeners if touchField does nothing', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	const listener = vi.fn(() => undefined)
-	form.store.subscribe(listener)
-	expect(listener).toHaveBeenCalledTimes(0)
-	form.touchField('a.b.c')
-	expect(listener).toHaveBeenCalledTimes(1)
-	form.touchField('a.b.c')
-	expect(listener).toHaveBeenCalledTimes(1)
-})
-
-it('registers and unregisters fields', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	const validator = () => null
-	expect((form.store.getState().values as any).b).toBe(undefined)
-	expect(form.store.getState().touched['b']).toBe(undefined)
-	expect(form.store.getState().errors['b']).toBe(undefined)
-	expect(form.store.getState().validators['b']).toBe(undefined)
-	const unregisterB = form.registerField('b', validator)
-	const unregisterC = form.registerField('c', undefined)
-	expect((form.store.getState().values as any).b).toBe(null)
-	expect(form.store.getState().touched['b']).toBe(false)
-	expect(form.store.getState().errors['b']).toBe(null)
-	expect(form.store.getState().validators['b']).toBe(validator)
-	expect(form.store.getState().validators['c']).toBe(null)
-	form.setFieldValue('b', 'value')
-	form.setFieldValue('c', 'value')
-	expect(getPath(form.store.getState().values, 'b')).toBe('value')
-	form.registerField('b', validator)
-	form.registerField('c', undefined)
-	expect(getPath(form.store.getState().values, 'b')).toBe('value')
-	unregisterB()
-	unregisterC()
-	expect((form.store.getState().values as any).b).toBe(undefined)
-	expect(form.store.getState().touched['b']).toBe(undefined)
-	expect(form.store.getState().errors['b']).toBe(undefined)
-	expect(form.store.getState().validators['b']).toBe(undefined)
-	expect(form.store.getState().validators['c']).toBe(undefined)
-})
-
-it('validates field', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	const validator = (value: unknown) => (value === 1 ? null : 'Error!')
-	form.registerField('a.b.c', validator)
-	expect(form.validateField('a.b.c')).toBe(false)
-	expect(form.store.getState().errors['a.b.c']).toBe('Error!')
-	form.setFieldValue('a.b.c', 1)
-	expect(form.validateField('a.b.c')).toBe(true)
-	expect(form.store.getState().errors['a.b.c']).toBe(null)
-})
-
-it('validates fields', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	const validator = (value: unknown) => (value === 1 ? null : 'Error!')
-	form.registerField('a.b.c', validator)
-	form.registerField('a.b.d', validator)
-	form.setFieldValue('a.b.c', 1)
-	expect(form.validateFields()).toBe(false)
-	expect(form.store.getState().errors['a.b.c']).toBe(null)
-	expect(form.store.getState().errors['a.b.d']).toBe('Error!')
-	form.setFieldValue('a.b.d', 1)
-	expect(form.validateFields()).toBe(true)
-	expect(form.store.getState().errors['a.b.c']).toBe(null)
-	expect(form.store.getState().errors['a.b.d']).toBe(null)
-})
-
-it('skips calling listeners when batching', () => {
-	const form = new Formulier({initialValues: INITIAL_VALUES})
-	const listener = vi.fn(() => undefined)
-	form.store.subscribe(listener)
-	expect(listener).toHaveBeenCalledTimes(0)
-	form.store.setState(state => ({...state, errors: {'a.b.c': 'Error 1'}}))
-	form.store.setState(state => ({...state, errors: {'a.b.c': 'Error 2'}}))
-	expect(listener).toHaveBeenCalledTimes(2)
-	form.store.batch(() => {
-		form.store.setState(state => ({...state, errors: {'a.b.c': 'Error 3'}}))
-		form.store.setState(state => ({...state, errors: {'a.b.c': 'Error 4'}}))
+describe('form', () => {
+	it('copies initial values', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		expect(form.store.getState().values).toBe(INITIAL_VALUES)
 	})
-	expect(listener).toHaveBeenCalledTimes(3)
+
+	it('calls listeners', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		const listener = vi.fn(() => undefined)
+		form.store.subscribe(listener)
+		form.setFieldValue('a.b.c', 'd')
+		expect(listener).toHaveBeenCalledTimes(1)
+	})
+
+	it('removes listeners', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		const listener = vi.fn(() => undefined)
+		const cancel = form.store.subscribe(listener)
+		cancel()
+		form.setFieldValue('a.b.c', 'd')
+		expect(listener).toHaveBeenCalledTimes(0)
+		expect(form.store['listeners'].size).toBe(0)
+	})
+
+	it('touches fields', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		expect(form.store.getState().touched['a.b.c']).toBe(undefined)
+		form.touchField('a.b.c')
+		expect(form.store.getState().touched['a.b.c']).toBe(true)
+	})
+
+	it('increments submit count', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		expect(form.store.getState().submitCount).toBe(0)
+		form.incrementSubmitCount()
+		form.incrementSubmitCount()
+		expect(form.store.getState().submitCount).toBe(2)
+	})
+
+	it('should not call listeners if setFieldValue does nothing', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		const listener = vi.fn(() => undefined)
+		form.store.subscribe(listener)
+		expect(listener).toHaveBeenCalledTimes(0)
+		form.setFieldValue('a.b.c', 'd')
+		expect(listener).toHaveBeenCalledTimes(1)
+		form.setFieldValue('a.b.c', 'd')
+		expect(listener).toHaveBeenCalledTimes(1)
+	})
+
+	it('should not call listeners if touchField does nothing', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		const listener = vi.fn(() => undefined)
+		form.store.subscribe(listener)
+		expect(listener).toHaveBeenCalledTimes(0)
+		form.touchField('a.b.c')
+		expect(listener).toHaveBeenCalledTimes(1)
+		form.touchField('a.b.c')
+		expect(listener).toHaveBeenCalledTimes(1)
+	})
+
+	it('registers and unregisters fields', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		const validator = () => null
+		expect((form.store.getState().values as any).b).toBe(undefined)
+		expect(form.store.getState().touched['b']).toBe(undefined)
+		expect(form.store.getState().errors['b']).toBe(undefined)
+		expect(form.store.getState().validators['b']).toBe(undefined)
+		const unregisterB = form.registerField('b', validator)
+		const unregisterC = form.registerField('c', undefined)
+		expect((form.store.getState().values as any).b).toBe(null)
+		expect(form.store.getState().touched['b']).toBe(false)
+		expect(form.store.getState().errors['b']).toBe(null)
+		expect(form.store.getState().validators['b']).toBe(validator)
+		expect(form.store.getState().validators['c']).toBe(null)
+		form.setFieldValue('b', 'value')
+		form.setFieldValue('c', 'value')
+		expect(getPath(form.store.getState().values, 'b')).toBe('value')
+		form.registerField('b', validator)
+		form.registerField('c', undefined)
+		expect(getPath(form.store.getState().values, 'b')).toBe('value')
+		unregisterB()
+		unregisterC()
+		expect((form.store.getState().values as any).b).toBe(undefined)
+		expect(form.store.getState().touched['b']).toBe(undefined)
+		expect(form.store.getState().errors['b']).toBe(undefined)
+		expect(form.store.getState().validators['b']).toBe(undefined)
+		expect(form.store.getState().validators['c']).toBe(undefined)
+	})
+
+	it('validates field', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		const validator = (value: unknown) => (value === 1 ? null : 'Error!')
+		form.registerField('a.b.c', validator)
+		expect(form.validateField('a.b.c')).toBe(false)
+		expect(form.store.getState().errors['a.b.c']).toBe('Error!')
+		form.setFieldValue('a.b.c', 1)
+		expect(form.validateField('a.b.c')).toBe(true)
+		expect(form.store.getState().errors['a.b.c']).toBe(null)
+	})
+
+	it('validates fields', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		const validator = (value: unknown) => (value === 1 ? null : 'Error!')
+		form.registerField('a.b.c', validator)
+		form.registerField('a.b.d', validator)
+		form.setFieldValue('a.b.c', 1)
+		expect(form.validateFields()).toBe(false)
+		expect(form.store.getState().errors['a.b.c']).toBe(null)
+		expect(form.store.getState().errors['a.b.d']).toBe('Error!')
+		form.setFieldValue('a.b.d', 1)
+		expect(form.validateFields()).toBe(true)
+		expect(form.store.getState().errors['a.b.c']).toBe(null)
+		expect(form.store.getState().errors['a.b.d']).toBe(null)
+	})
+
+	it('skips calling listeners when batching', () => {
+		const form = new Formulier({initialValues: INITIAL_VALUES})
+		const listener = vi.fn(() => undefined)
+		form.store.subscribe(listener)
+		expect(listener).toHaveBeenCalledTimes(0)
+		form.store.setState(state => ({...state, errors: {'a.b.c': 'Error 1'}}))
+		form.store.setState(state => ({...state, errors: {'a.b.c': 'Error 2'}}))
+		expect(listener).toHaveBeenCalledTimes(2)
+		form.store.batch(() => {
+			form.store.setState(state => ({...state, errors: {'a.b.c': 'Error 3'}}))
+			form.store.setState(state => ({...state, errors: {'a.b.c': 'Error 4'}}))
+		})
+		expect(listener).toHaveBeenCalledTimes(3)
+	})
 })
--- packages/core/tests/state-utils.test.ts
+++ packages/core/tests/state-utils.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import {describe, expect, it} from 'vitest'
 
 import {getPath, isInteger, isObject, removeKey, setKey, setPath, toPath} from '../src/state-utils.js'
@@ -8,70 +10,90 @@ const SOURCE = {
 	z: 1,
 }
 
-it('getPath', () => {
-	expect(getPath(SOURCE, 'a.b')).toBe(SOURCE.a.b)
-	expect(getPath(SOURCE, 'a.b.c')).toBe(SOURCE.a.b.c)
-	expect(getPath(SOURCE, 'a.b.f')).toStrictEqual(undefined)
-	expect(getPath(SOURCE, 'a.b.f', 'fallback')).toStrictEqual('fallback')
-	expect(getPath(SOURCE, 'y[1][2]', 'fallback')).toStrictEqual('fallback')
-})
+describe('state utils', () => {
+	describe('getPath', () => {
+		it('gets stuff', () => {
+			expect(getPath(SOURCE, 'a.b')).toBe(SOURCE.a.b)
+			expect(getPath(SOURCE, 'a.b.c')).toBe(SOURCE.a.b.c)
+		})
 
-describe('setPath', () => {
-	it('sets stuff', () => {
-		expect(setPath(SOURCE, 'a.b.c', 'f').a.b.c).toBe('f')
+		it('returns undefined if path is invalid', () => {
+			expect(getPath(SOURCE, 'a.b.f')).toStrictEqual(undefined)
+		})
+
+		it('returns the provided fallback if path is invalid', () => {
+			expect(getPath(SOURCE, 'a.b.f', 'fallback')).toStrictEqual('fallback')
+			expect(getPath(SOURCE, 'y[1][2]', 'fallback')).toStrictEqual('fallback')
+		})
 	})
 
-	it('skips if value is indifferent', () => {
-		expect(setPath(SOURCE, 'a.b.c', 'c')).toBe(SOURCE)
+	describe('setPath', () => {
+		it('sets stuff', () => {
+			expect(setPath(SOURCE, 'a.b.c', 'f').a.b.c).toBe('f')
+		})
+
+		it('skips if value is indifferent', () => {
+			expect(setPath(SOURCE, 'a.b.c', 'c')).toBe(SOURCE)
+		})
+
+		it('skips if value is indifferent', () => {
+			expect(setPath(SOURCE, 'a.b.c', 'c')).toBe(SOURCE)
+		})
+
+		it('unsets if value is undefined', () => {
+			expect(setPath(SOURCE, 'a.b.c', undefined).a.b).toStrictEqual({})
+			expect(setPath(SOURCE, 'a', undefined)).toStrictEqual({y: 'test', z: 1})
+		})
+
+		it('creates objects and arrays', () => {
+			expect(setPath(SOURCE, 'z[0].a', 1).z).toStrictEqual([{a: 1}])
+		})
 	})
 
-	it('skips if value is indifferent', () => {
-		expect(setPath(SOURCE, 'a.b.c', 'c')).toBe(SOURCE)
+	describe('setKey', () => {
+		it('works', () => {
+			expect(setKey(SOURCE, 'z', 1)).toBe(SOURCE)
+			expect(setKey(SOURCE, 'z', 2)).toStrictEqual({a: {b: {c: 'c'}}, y: 'test', z: 2})
+		})
 	})
 
-	it('unsets if value is undefined', () => {
-		expect(setPath(SOURCE, 'a.b.c', undefined).a.b).toStrictEqual({})
-		expect(setPath(SOURCE, 'a', undefined)).toStrictEqual({y: 'test', z: 1})
+	describe('removeKey', () => {
+		it('works', () => {
+			expect(removeKey(SOURCE, 'x')).toBe(SOURCE)
+			expect(removeKey(SOURCE, 'a')).toStrictEqual({y: 'test', z: 1})
+		})
 	})
 
-	it('creates objects and arrays', () => {
-		expect(setPath(SOURCE, 'z[0].a', 1).z).toStrictEqual([{a: 1}])
+	describe('toPath', () => {
+		it('works', () => {
+			expect(toPath('a.b')).toStrictEqual(['a', 'b'])
+			expect(toPath('a["b"]')).toStrictEqual(['a', 'b'])
+			expect(toPath("a['b']")).toStrictEqual(['a', 'b'])
+			expect(toPath('a[2]')).toStrictEqual(['a', '2'])
+			expect(toPath('a.[2]')).toStrictEqual(['a', '2'])
+			expect(toPath('a.[2][1].b')).toStrictEqual(['a', '2', '1', 'b'])
+			expect(toPath('a.[2].b')).toStrictEqual(['a', '2', 'b'])
+			expect(toPath('.[2]')).toStrictEqual(['', '2'])
+		})
 	})
-})
 
-it('setKey', () => {
-	expect(setKey(SOURCE, 'z', 1)).toBe(SOURCE)
-	expect(setKey(SOURCE, 'z', 2)).toStrictEqual({a: {b: {c: 'c'}}, y: 'test', z: 2})
-})
+	describe('isInteger', () => {
+		it('works', () => {
+			expect(isInteger('2')).toBe(true)
+			expect(isInteger('2.26')).toBe(false)
+			expect(isInteger(2)).toBe(false)
+		})
+	})
 
-it('removeKey', () => {
-	expect(removeKey(SOURCE, 'x')).toBe(SOURCE)
-	expect(removeKey(SOURCE, 'a')).toStrictEqual({y: 'test', z: 1})
-})
-
-it('toPath', () => {
-	expect(toPath('a.b')).toStrictEqual(['a', 'b'])
-	expect(toPath('a["b"]')).toStrictEqual(['a', 'b'])
-	expect(toPath("a['b']")).toStrictEqual(['a', 'b'])
-	expect(toPath('a[2]')).toStrictEqual(['a', '2'])
-	expect(toPath('a.[2]')).toStrictEqual(['a', '2'])
-	expect(toPath('a.[2][1].b')).toStrictEqual(['a', '2', '1', 'b'])
-	expect(toPath('a.[2].b')).toStrictEqual(['a', '2', 'b'])
-	expect(toPath('.[2]')).toStrictEqual(['', '2'])
-})
-
-it('isInteger', () => {
-	expect(isInteger('2')).toBe(true)
-	expect(isInteger('2.26')).toBe(false)
-	expect(isInteger(2)).toBe(false)
-})
-
-it('isObject', () => {
-	expect(isObject({})).toBe(true)
-	expect(isObject([])).toBe(true)
-	expect(isObject(new Date())).toBe(true)
-	expect(isObject(null)).toBe(false)
-	expect(isObject(undefined)).toBe(false)
-	expect(isObject(1)).toBe(false)
-	expect(isObject('test')).toBe(false)
+	describe('isObject', () => {
+		it('works', () => {
+			expect(isObject({})).toBe(true)
+			expect(isObject([])).toBe(true)
+			expect(isObject(new Date())).toBe(true)
+			expect(isObject(null)).toBe(false)
+			expect(isObject(undefined)).toBe(false)
+			expect(isObject(1)).toBe(false)
+			expect(isObject('test')).toBe(false)
+		})
+	})
 })
